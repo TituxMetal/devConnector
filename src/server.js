df943195d48b678412b70ef5d69836b0414f68ab
Laptop | Lang Guillaume <lang.guillaume@gmail.com>
2018-05-12 05:19:43 +0200

    Add body parser

--- src/server.js
+++ src/server.js
@@ -1,12 +1,18 @@
 const env = require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
 const users = require('./routes/api/users')
 const profile = require('./routes/api/profile')
 const posts = require('./routes/api/posts')
 
 const app = express()
+
+// Body parser middlewares
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
  // DB Config
 const db = process.env.mongodbURI
 
