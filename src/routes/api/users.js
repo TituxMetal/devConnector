d209da33dcbb4f547819666448cad2e5b9ca2beb
Laptop | Lang Guillaume <lang.guillaume@gmail.com>
2018-05-12 05:43:59 +0200

    Add login route

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -19,37 +19,62 @@ router.get('/test', (req, res) => res.json({ msg: "Users Works" }))
   @access   Public
 */
 router.post('/register', (req, res) => {
-  User.findOne({ email: req.body.email })
-    .then(user => {
-      if (user) {
-        return res.status(400).json({ email: 'Email already exists' })
-      }
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: 'Email already exists' })
+    }
 
-      const avatar = gravatar.url(req.body.email, {
-        s: 200, // Size
-        r: 'pg', // Rating
-        d: 'mm' // Default
-      })
-      const newUser = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-        avatar
-      })
+    const avatar = gravatar.url(req.body.email, {
+      s: 200, // Size
+      r: 'pg', // Rating
+      d: 'mm' // Default
+    })
+    const newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      avatar
+    })
 
-      bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(newUser.password, salt, (err, hash) => {
-          if (err) {
-            throw err
-          }
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if (err) {
+          throw err
+        }
 
-          newUser.password = hash
-          newUser.save()
-            .then(user => res.json(user))
-            .catch(err => console.log(err))
-        })
+        newUser.password = hash
+        newUser.save()
+          .then(user => res.json(user))
+          .catch(err => console.log(err))
       })
     })
+  })
+})
+
+/*
+  @route    POST api/users/login
+  @desc     Login user / Returning JWT Token
+  @access   Public
+*/
+router.post('/login', (req, res) => {
+  const email = req.body.email
+  const password = req.body.password
+
+  // Find user by email
+  User.findOne({ email }).then(user => {
+    if (!user) {
+      return res.status(404).json({ email: 'User not found' })
+    }
+
+    // Check password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (!isMatch) {
+        return res.status(400).json({ password: 'Password incorrect' })
+      }
+
+      return res.json({ msg: 'Success' })
+    })
+  })
 })
 
 module.exports = router
