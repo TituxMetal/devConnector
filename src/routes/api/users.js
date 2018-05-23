6e2d8d4ae703bba0cff6c6352c75a5f984da9b66
Laptop | Lang Guillaume <lang.guillaume@gmail.com>
2018-05-23 07:41:12 +0200

    Update users routes

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const router = require('express-promise-router')()
 
-router.get('/itWorks', (req, res, next) => res.json({ msg: "Users Works" }))
+const { schemas } = require('../../validation/users')
+const validateBody = require('../../middlewares/validateBody')(schemas.register)
+const UserController = require('../../controllers/api/users')
+
+router.post('/register', validateBody, UserController.register)
+router.get('/itWorks', UserController.itWorks)
+
 
 module.exports = router
