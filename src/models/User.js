26bb1e2ba81a9c28bcd9936f37846bc863c55993
Laptop | Lang Guillaume <lang.guillaume@gmail.com>
2018-05-24 20:18:05 +0200

    Update users model; hash the password before saving the user

--- src/models/User.js
+++ src/models/User.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 const Schema = mongoose.Schema
 
-const UserSchema = new Schema({
+const userSchema = new Schema({
   name: {
     type: String,
     required: true
@@ -24,4 +25,24 @@ const UserSchema = new Schema({
   }
 })
 
-module.exports = User = mongoose.model('users', UserSchema)
+userSchema.pre('save', async function (next) {
+  try {
+    const salt = await bcrypt.genSalt(10)
+    const passwordHash = await bcrypt.hash(this.password, salt)
+
+    this.password = passwordHash
+    next()
+  } catch(error) {
+    next(error)
+  }
+})
+
+userSchema.statics.findByEmail = async email => {
+  try {
+    return await User.findOne({ email })
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+module.exports = User = mongoose.model('users', userSchema)
