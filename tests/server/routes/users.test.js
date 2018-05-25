ad8107e2ca25b2e7660ebae33962138656e25637
Laptop | Lang Guillaume <lang.guillaume@gmail.com>
2018-05-25 19:37:56 +0200

    Add tests for users login route

--- tests/server/routes/users.test.js
+++ tests/server/routes/users.test.js
@@ -9,18 +9,20 @@ const User = require(root + '/models/User')
 
 describe('Users route', () => {
   const register = '/api/users/register'
+  const login = '/api/users/login'
   const test = '/api/users/itWorks'
-  const user = {name: faker.name.findName(), email: faker.internet.email(), password: faker.internet.password() }
-  const preSave = { name: 'Test name', email: 'sometest@gmail.com', password: 'testPassword'}
+  const user = { email: faker.internet.email(), password: faker.internet.password() }
+  const preSave = { email: 'sometest@gmail.com', password: 'testPassword'}
 
   beforeAll(async () => {
+    const testUser = { ...preSave }
+    testUser.name = 'Test name'
     const response = await request(server)
       .post(register)
-      .send(preSave)
-    
+      .send(testUser)
+
     expect(response.status).toEqual(200)
     expect(response.body).toExist
-    expect(response.body.password).not.toBe(preSave.password)
   })
 
   afterAll(async () => {
@@ -31,22 +33,23 @@ describe('Users route', () => {
   
   describe('POST /api/users/register', () => {
     it('should create new user if email not exists', async () => {
-      const res = await request(server).post(register).send(user)
-      
+      const testUser = { ...user }
+      testUser.name = faker.name.findName()
+      const res = await request(server).post(register).send(testUser)
+
       expect(res.status).toEqual(200)
-      expect(res.body.name).toEqual(user.name)
-      expect(res.body.email).toEqual(user.email)
+      expect(res.body.name).toEqual(testUser.name)
+      expect(res.body.email).toEqual(testUser.email)
       expect(res.body.password).toNotExist
 
       const newUser = await User.find({ email: res.body.email })
       expect(newUser.length).toBe(1)
     })
 
-    it('should create a new user with a hashed password', async () => {
-      await request(server).post(register).send(user)
+    it('password should be a hashed password', async () => {
+      const savedUser = await User.findByEmail(preSave.email)
 
-      const savedUser = await User.findByEmail(user.email)
-      expect(savedUser.password).not.toEqual(user.password)
+      expect(savedUser.password).not.toEqual(preSave.password)
     })
 
     it('should return 400 if no password given', async () => {
@@ -56,7 +59,7 @@ describe('Users route', () => {
       expect(res.status).toEqual(400)
       expect(res.body.errors.password).toExist
       expect(res.body.errors.password).toEqual(`"Password field" is required`)
-      
+
       const noUser = await User.findByEmail(invalidUser.email)
       expect(noUser).toBe(null)
     })
@@ -70,15 +73,17 @@ describe('Users route', () => {
       expect(res.body.errors.name).toEqual(`"Name field" length must be at least 4 characters long`)
       expect(res.body.errors.email).toEqual(`"Email field" must be a valid email`)
       expect(res.body.errors.password).toEqual(`"Password field" length must be at least 6 characters long`)
-      
+
       const noUser = await User.findByEmail(invalidUser.email)
       expect(noUser).toBe(null)
     })
-    
-    it('should return 400 if email already in use', async () => {
-      const res = await request(server).post(register).send(preSave)
 
-      const existingUser = await User.find({ email: preSave.email })
+    it('should return 400 if email already in use', async () => {
+      const existUser = { ...preSave }
+      existUser.name = 'Fake Name'
+      const res = await request(server).post(register).send(existUser)
+
+      const existingUser = await User.find({ email: existUser.email })
       expect(existingUser.length).toBe(1)
 
       expect(res.status).toEqual(400)
@@ -86,6 +91,42 @@ describe('Users route', () => {
       expect(res.body.errors.email).toEqual('Email already exists')
     })
   })
+
+  describe('POST /api/users/login', () => {
+    it('should return 400 if user email and password empty', async () => {
+      const emptyUser = {}
+      const res = await request(server).post(login).send(emptyUser)
+
+      expect(res.status).toEqual(400)
+      expect(res.body.errors.email).toEqual(`"Email field" is required`)
+      expect(res.body.errors.password).toEqual(`"Password field" is required`)
+    })
+
+    it('shoult return 400 if invalid user email', async () => {
+      const invalidUserEmail = { ...preSave }
+      invalidUserEmail.email = 'invalid@mail.com'
+      const res = await request(server).post(login).send(invalidUserEmail)
+  
+      expect(res.status).toEqual(400)
+      expect(res.body.errors.auth).toEqual('Incorrect email or password')
+    })
+
+    it('shoult return 400 if invalid user password', async () => {
+      const invalidUserPassword = { ...preSave }
+      invalidUserPassword.password = 'invalidPassword'
+      const res = await request(server).post(login).send(invalidUserPassword)
+
+      expect(res.status).toEqual(400)
+      expect(res.body.errors.auth).toEqual('Incorrect email or password')
+    })
+
+    it('should return 200 and token if valid credentials', async () => {
+      const res = await request(server).post(login).send(preSave)
+      expect(res.status).toEqual(200)
+      expect(res.body.token).toExist
+    })
+  })
+
 
   describe('POST /api/users/itWorks', () => {
     it('should return 200 and message Users Works', async () => {
