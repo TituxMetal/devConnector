034d0867cf8607a2d8fc62c942c8490df28b8944
Laptop | Lang Guillaume <lang.guillaume@gmail.com>
2018-05-25 20:00:07 +0200

    Add the route for login user

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -2,10 +2,12 @@ const express = require('express')
 const router = require('express-promise-router')()
 
 const { schemas } = require('../../validation/users')
-const validateBody = require('../../middlewares/validateBody')(schemas.register)
+const validateRegister = require('../../middlewares/validateBody')(schemas.register)
+const validateLogin = require('../../middlewares/validateBody')(schemas.login)
 const UserController = require('../../controllers/api/users')
 
-router.post('/register', validateBody, UserController.register)
+router.post('/register', validateRegister, UserController.register)
+router.post('/login', validateLogin, UserController.login)
 router.get('/itWorks', UserController.itWorks)
 
 
