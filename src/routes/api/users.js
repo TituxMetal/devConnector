ccb3fa9ebf1572f4b56b50f18df3320341edf503
Laptop | Lang Guillaume <lang.guillaume@gmail.com>
2018-05-27 06:54:04 +0200

    Add the current users route

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const router = require('express-promise-router')()
+const passport = require('passport')
+const passportConf = require('../../passport/strategy')
 
 const { schemas } = require('../../validation/users')
 const validateRegister = require('../../middlewares/validateBody')(schemas.register)
 const validateLogin = require('../../middlewares/validateBody')(schemas.login)
 const UserController = require('../../controllers/api/users')
+const passportJWT = passport.authenticate('jwt', { session: false })
 
 router.post('/register', validateRegister, UserController.register)
 router.post('/login', validateLogin, UserController.login)
+router.get('/current', passportJWT, UserController.current)
 router.get('/itWorks', UserController.itWorks)
 
 
