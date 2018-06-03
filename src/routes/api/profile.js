2bedc2dedffbe1614591aad479a63a7dfd0c6c26
Laptop | Lang Guillaume <lang.guillaume@gmail.com>
2018-06-03 06:21:47 +0200

    Update profile routes; Add POST /api/profile route

--- src/routes/api/profile.js
+++ src/routes/api/profile.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const router = require('express-promise-router')()
+const passport = require('passport')
+const passportConf = require('../../passport/strategy')
 
+const { schemas } = require('../../validation/profile')
+const validateProfile = require('../../middlewares/validateBody')(schemas.profile)
 const ProfileController = require('../../controllers/api/profile')
+const passportJWT = passport.authenticate('jwt', { session: false })
 
+router.post('/', passportJWT, validateProfile, ProfileController.create)
 router.get('/itWorks', ProfileController.itWorks)
 
 module.exports = router
