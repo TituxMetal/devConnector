cf7a0f130508279d2e09bad671a4bd1183bbcf67
Laptop | Lang Guillaume <lang.guillaume@gmail.com>
2018-06-07 19:42:44 +0200

    Refactor tests for profile to make them less dependent on each other

--- tests/server/routes/profile.test.js
+++ tests/server/routes/profile.test.js
@@ -7,31 +7,60 @@ const root = path.join(__dirname, '../../../src')
 const server = require(root + '/server')
 const User = require(root + '/models/User')
 const Profile = require(root + '/models/Profile')
-let userId
+let userPassword = faker.internet.password()
+let users = []
+let profiles = []
 let token
 
 describe('Profile route', () => {
-  const test = '/api/profile/itWorks'
-  const rootUri = '/api/profile/'
-  const fakeUser = {
-    name: 'Fake User',
-    email: faker.internet.email(),
-    password: faker.internet.password()
+  const uri = '/api/profile'
+  const test = `${uri}/itWorks`
+  const getAll = `${uri}/all`
+
+  const dropDatabase = async () => {
+    await mongoose.connection.dropDatabase()
+    console.log('\n Test database dropped')
   }
+
+  const dropCollection = async (name) => {
+    await mongoose.connection.dropCollection(name)
+  }
+
+  const createUser = async () => {
+    const user = await new User({
+      name: faker.internet.userName(),
+      email: faker.internet.email(),
+      password: userPassword
+    }).save()
+    users.push(user)
+  }
+
+  const createProfile = async (userId, handle) => {
+    const profile = await new Profile({
+      user: userId,
+      handle: handle,
+      status: 'testStatus',
+      skills: 'php, html, css',
+      bio: 'Fake biography for the test'
+    }).save()
+    profiles.push(profile)
+  }
+  
   const fakeProfile = {
-    handle: 'fakeHandle',
     status: 'test',
     skills: 'php, css, html',
     bio: 'Fake biography'
   }
 
   beforeAll(async () => {
-    const user = await new User(fakeUser).save()
-    userId = user.id
-    const login = '/api/users/login'
+    await createUser()
+    console.log(`User Created`)
+    const user = users[0]
 
-    const res = await request(server).post(login).send({ email: fakeUser.email, password: fakeUser.password })
+    const login = '/api/users/login'
+    const res = await request(server).post(login).send({ email: user.email, password: userPassword })
     token = `Bearer ${res.body.token}`
+    console.log('User Logged in')
   })
 
   afterAll(async () => {
@@ -41,16 +70,18 @@ describe('Profile route', () => {
   })
 
   describe('POST /api/profile/', () => {
+    beforeAll(async () => profiles = [])
+    afterAll(async () => await dropCollection('profiles'))
+
     it('should return 401 if no token header given', async () => {
-      const profile = { ...fakeProfile }
-      const res = await request(server).post(rootUri).send(profile)
+      const res = await request(server).post(uri).send({})
 
       expect(res.status).toBe(401)
     })
 
     it('should return 400 if missing required fields', async () => {
       const requiredFields = ['handle', 'status', 'skills', 'bio']
-      const res = await request(server).post(rootUri).send({}).set('Authorization', token)
+      const res = await request(server).post(uri).send({}).set('Authorization', token)
 
       expect(res.status).toEqual(400)
       requiredFields.map(field => {
@@ -60,8 +91,8 @@ describe('Profile route', () => {
     })
 
     it('should return 400 if invalid website given', async () => {
-      const profile = { ...fakeProfile, website: 'invalid' }
-      const res = await request(server).post(rootUri).send(profile).set('Authorization', token)
+      const profile = { handle: 'test', ...fakeProfile, website: 'invalid' }
+      const res = await request(server).post(uri).send(profile).set('Authorization', token)
       
       expect(res.status).toEqual(400)
       expect(res.body.errors.website).toEqual(`"Website field" must be a valid uri`)
@@ -73,8 +104,8 @@ describe('Profile route', () => {
       socialFields.map(field => {
         social[field] = 'test'
       })
-      const profile = { ...fakeProfile, social }
-      const res = await request(server).post(rootUri).send(profile).set('Authorization', token)
+      const profile = { handle: 'test', ...fakeProfile, social }
+      const res = await request(server).post(uri).send(profile).set('Authorization', token)
 
       expect(res.status).toEqual(400)
       socialFields.map(field => {
@@ -90,7 +121,7 @@ describe('Profile route', () => {
         invalidFields[field] = 'ab'
       })
       const profile = { ...invalidFields }
-      const res = await request(server).post(rootUri).send(profile).set('Authorization', token)
+      const res = await request(server).post(uri).send(profile).set('Authorization', token)
 
       expect(res.status).toEqual(400)
       fields.map(field => {
@@ -101,18 +132,20 @@ describe('Profile route', () => {
 
     it('should return 400 if handle length is up to 40 characters long', async () => {
       const longHandle = 'loremipsumdolorsitametconsecteturadipisic'
-      const { handle, status, skills, bio } = { ...fakeProfile }
+      const { status, skills, bio } = { ...fakeProfile }
       const profile = { handle: longHandle, status, skills, bio }
-      const res = await request(server).post(rootUri).send(profile).set('Authorization', token)
+      const res = await request(server).post(uri).send(profile).set('Authorization', token)
 
       expect(res.status).toEqual(400)
       expect(res.body.errors.handle).toEqual(`"Handle field" length must be less than or equal to 40 characters long`)
     })
 
     it('should create a profile in database', async () => {
-      const profile = { ...fakeProfile }
-      const res = await request(server).post(rootUri).send(profile).set('Authorization', token)
+      const profile = { handle: 'test', ...fakeProfile }
+      const userId = users[0].id
+      const res = await request(server).post(uri).send(profile).set('Authorization', token)
       profile.skills = profile.skills.split(',').map(skill => skill.trim())
+      profiles.push(profile)
       
       expect(res.status).toEqual(200)
       expect(res.body.user).toBeDefined()
@@ -122,7 +155,7 @@ describe('Profile route', () => {
       expect(res.body.status).toEqual(profile.status)
       
       const userProfile = await Profile.findByUser(userId)
-
+      
       expect(userProfile.handle).toEqual(profile.handle)
       expect(userProfile.status).toEqual(profile.status)
       expect(userProfile.bio).toEqual(profile.bio)
@@ -131,22 +164,19 @@ describe('Profile route', () => {
     })
 
     it('should return 400 if profile already exists', async () => {
-      const profile = { ...fakeProfile }
-      const res = await request(server).post(rootUri).send(profile).set('Authorization', token)
+      const profile = { handle: 'test', ...fakeProfile }
+      const res = await request(server).post(uri).send(profile).set('Authorization', token)
 
       expect(res.status).toBe(400)
       expect(res.body.errors.profile).toEqual('Profile already exists for this user')
     })
 
     it('should return 400 if handle is already in use', async () => {
-      const newUser = {
-        name: 'Test User',
-        email: faker.internet.email(),
-        password: faker.internet.password()
-      }
-      await new User(newUser).save()
-      const userLogin = await request(server).post('/api/users/login').send({ email: newUser.email, password: newUser.password })
-      const res = await request(server).post(rootUri).send(fakeProfile).set('Authorization', `Bearer ${userLogin.body.token}`)
+      await createUser()
+      const newUser = users[users.length - 1]
+      const userLogin = await request(server).post('/api/users/login').send({ email: newUser.email, password: userPassword })
+      const profile = { handle: 'test', ...fakeProfile }
+      const res = await request(server).post(uri).send(profile).set('Authorization', `Bearer ${userLogin.body.token}`)
 
       expect(res.status).toBe(400)
       expect(res.body.errors.handle).toEqual(`Handle already in use`)
@@ -154,63 +184,74 @@ describe('Profile route', () => {
   })
 
   describe('GET /api/profile', () => {
+    beforeAll(async () => profiles = [])
+    afterAll(async () => await dropCollection('profiles'))
+
     it('should return the current logged in user profile', async () => {
-      const res = await request(server).get(rootUri).set('Authorization', token)
-      const skillsArray = fakeProfile.skills.split(',').map(skill => skill.trim())
+      await createProfile(users[0].id, 'test')
+      profile = profiles[profiles.length - 1]
+      const res = await request(server).get(uri).set('Authorization', token)
 
       expect(res.status).toBe(200)
-      expect(res.body.handle).toEqual(fakeProfile.handle)
-      expect(res.body.status).toEqual(fakeProfile.status)
-      expect(res.body.skills).toEqual(skillsArray)
-      expect(res.body.bio).toEqual(fakeProfile.bio)
+      expect(res.body.handle).toEqual('test')
+      expect(res.body.status).toEqual(profile.status)
+      expect(res.body.skills).toEqual(expect.arrayContaining(profile.skills))
+      expect(res.body.bio).toEqual(profile.bio)
     })
 
     it('should return 404 if no user profile found', async () => {
-      const newUser = {
-        name: 'test user',
-        email: faker.internet.email(),
-        password: faker.internet.password()
-      }
-      await new User(newUser).save()
-      const userToken = await request(server).post('/api/users/login').send({ email: newUser.email, password: newUser.password })
-      const res = await request(server).get(rootUri).set('Authorization', `Bearer ${userToken.body.token}`)
+      await createUser()
+      const newUser = users[users.length - 1]
+      const userToken = await request(server).post('/api/users/login').send({ email: newUser.email, password: userPassword })
+      const res = await request(server).get(uri).set('Authorization', `Bearer ${userToken.body.token}`)
 
       expect(res.status).toBe(404)
       expect(res.body.errors.profile).toEqual('No profile found')
     })
 
     it('should return 401 if no token header given', async () => {
-      const res = await request(server).get(rootUri)
+      const res = await request(server).get(uri)
 
       expect(res.status).toBe(401)
     })
   })
 
   describe('PUT /api/profile/', () => {
+    beforeAll(async () => profiles = [])
+    afterAll(async () => await dropCollection('profiles'))
+
     it('should return 404 if profile not found', async () => {
-      const newUser = { name: 'test', email: 'test@test.com', password: 'testuser' }
-      await new User(newUser).save()
-      const token = await request(server).post('/api/users/login').send({ email: newUser.email, password: newUser.password })
+      await createUser()
+      const newUser = users[users.length -1]
+      const token = await request(server).post('/api/users/login').send({ email: newUser.email, password: userPassword })
       const profile = { ...fakeProfile }
       profile.handle = 'testhandle'
-      const res = await request(server).put(rootUri).send(profile).set('Authorization', `Bearer ${token.body.token}`)
+      const res = await request(server).put(uri).send(profile).set('Authorization', `Bearer ${token.body.token}`)
 
       expect(res.status).toBe(404)
       expect(res.body.errors.profile).toEqual('Profile not found')
     })
 
     it('should edit the profile', async () => {
-      const profile = { ...fakeProfile }
-      profile.status = 'updated status'
-      profile.website = 'http://fake.com'
-      profile.social = { twitter: 'https://twitter.com/test' }
-      const res = await request(server).put(rootUri).send(profile).set('Authorization', token)
+      await createProfile(users[0].id, 'test')
+      const p = profiles[profiles.length - 1]
+      const profile = {
+        handle: 'testHandle',
+        status: 'updated status',
+        website: 'http://fake.com',
+        social: { twitter: 'https://twitter.com/test' },
+        skills: 'php, javascript, react',
+        bio: p.bio
+      }
+      const res = await request(server).put(uri).send(profile).set('Authorization', token)
 
       expect(res.status).toBe(200)
       expect(res.body.handle).toEqual(profile.handle)
       expect(res.body.status).toEqual(profile.status)
       expect(res.body.website).toEqual(profile.website)
       expect(res.body.social).toEqual(profile.social)
+      expect(res.body.skills.join()).toEqual(profile.skills)
+      expect(res.body.bio).toEqual(profile.bio)
     })
   })
 
