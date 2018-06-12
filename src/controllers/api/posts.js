3a6149b2fcb8cfaaed59b45926a2f1269c46032b
Laptop | Lang Guillaume <lang.guillaume@gmail.com>
2018-06-12 16:04:38 +0200

    Update posts controller; add method getAll that get all posts

--- src/controllers/api/posts.js
+++ src/controllers/api/posts.js
@@ -1,7 +1,18 @@
+const Post = require('../../models/Post')
 
 const PostController = {
   itWorks: (req, res, next) => {
     return res.status(200).json({ msg: "Posts Works" })
+  },
+
+  getAll: async (req, res, next) => {
+    const posts = await Post.find().sort({ date: -1 })
+
+    if (posts.length === 0) {
+      return res.status(404).json({ errors: { posts: 'There are no posts' } })
+    }
+
+    return res.status(200).json(posts)
   }
 }
 
