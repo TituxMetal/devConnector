74491ee2ea47a1ee715f12fb437d0abda365e8fa
Laptop | Lang Guillaume <lang.guillaume@gmail.com>
2018-06-12 19:30:47 +0200

    Update posts routes; add POST /api/posts route

--- src/routes/api/posts.js
+++ src/routes/api/posts.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const router = require('express-promise-router')()
+const passport = require('passport')
+const passportConf = require('../../passport/strategy')
 
+const { schemas } = require('../../validation/post')
+const validatePost = require('../../middlewares/validateBody')(schemas.post)
 const PostController = require('../../controllers/api/posts')
+const passportJWT = passport.authenticate('jwt', { session: false })
 
 router.get('/', PostController.getAll)
+router.post('/', passportJWT, validatePost, PostController.create)
 router.get('/itWorks', PostController.itWorks)
 router.get('/:postId', PostController.getId)
 
