5705638dcc544ff3ea5e191410bc2efe3e70ddf2
Laptop | Lang Guillaume <lang.guillaume@gmail.com>
2018-06-15 19:38:05 +0200

    Update post validation; add validation for comments fields

--- src/validation/post.js
+++ src/validation/post.js
@@ -6,6 +6,12 @@ const schemas = {
     text: Joi.string().required().min(10).max(400).label('Text field').trim(),
     name: Joi.string().optional().min(4).label('Name field').trim(),
     avatar: Joi.string().optional().uri().label('Avatar field').trim()
+  }),
+  comment: Joi.object().keys({
+    user: Joi.string(),
+    text: Joi.string().required().min(10).max(400).label('Text field').trim(),
+    name: Joi.string().optional().min(4).label('Name field').trim(),
+    avatar: Joi.string().optional().uri().label('Avatar field').trim()
   })
 }
 
